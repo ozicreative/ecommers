391cb9236fdd24e7441c72b245c760255cfc101b
ozicreative <rozbalens@gmail.com>
2020-11-27 00:39:02 +0700

    commit

---
+++ app/Database/Migrations/2020-11-26-173717_transaksi_alter_alamat.php
@@ -0,0 +1,31 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TransaksiAlterAlamat extends Migration
+{
+	public function up()
+	{
+		$fields = [
+			'alamat'=>[
+				'type'=>'TEXT'
+			],
+			'ongkir'=>[
+				'type'=>'INT',
+			],
+			'status'=>[
+				'type'=>'INT',
+				'constraint'=>1,
+			],
+		];
+
+		$this->forge->addColumn('transaksi', $fields);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropColumn('transaksi', ['alamat','ongkir','status']);
+	}
+}
